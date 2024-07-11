deea8cd9a1915f17c742356a854d3b400622deb4
Vishal rathore <130865513+vishalgit1810@users.noreply.github.com>
2024-07-11 22:24:59 +0530

    Update get-weather-controller.js

--- src/controllers/get-weather-controller.js
+++ src/controllers/get-weather-controller.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getWeather = async (req, res) => {
     const city = req.query.city;
-    const apiKey = "your_api_key";
+    const apiKey = "9b0b2f74557903020464ff9d946bd95a";
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
